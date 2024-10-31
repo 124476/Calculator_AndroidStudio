0f0c0c22f74068ae21cc17fa264f303b36019c75
124476 <124476@bk.ru>
2024-10-31 21:43:45 +0300

    Uplode toast and alert message

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.calculator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,22 +26,37 @@ public class MainActivity extends AppCompatActivity {
         editNumberOne = findViewById(R.id.editNumberOne);
         editNumberTwo = findViewById(R.id.editNumberTwo);
         buttonAdd = findViewById(R.id.buttonAdd);
+    }
 
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try
-                {
-                    float num1 = Float.parseFloat(editNumberOne.getText().toString());
-                    float num2 = Float.parseFloat(editNumberTwo.getText().toString());
-                    float res = num1 + num2;
-                    resultTextView.setText(String.valueOf(res));
+    public void buttonAddClick(View v) {
+        try
+        {
+            float num1 = Float.parseFloat(editNumberOne.getText().toString());
+            float num2 = Float.parseFloat(editNumberTwo.getText().toString());
+            float res = num1 + num2;
+            resultTextView.setText(String.valueOf(res));
+
+            Toast.makeText(this,"Accept", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception exception)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Log");
+            builder.setMessage("Error");
+            builder.setCancelable(true);
+            builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
                 }
-                catch (Exception exception)
-                {
-                    resultTextView.setText("Error");
-                };
-            }
-        });
+            });
+            builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.show();
+        };
     }
 }
